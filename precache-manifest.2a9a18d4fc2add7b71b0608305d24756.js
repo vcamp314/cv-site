52d9577268c2337ec40865685ef68821e75ed221
V.C <vitorrc314@gmail.com>
2022-07-09 18:02:45 +0900

    Updates

---
+++ precache-manifest.2a9a18d4fc2add7b71b0608305d24756.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f926657532d9efaace5cba509346f853",
+    "url": "/cv-site/index.html"
+  },
+  {
+    "revision": "8150923869619a0ae3c6",
+    "url": "/cv-site/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b551a0bf3916520b5608",
+    "url": "/cv-site/static/css/main.89d9a7fe.chunk.css"
+  },
+  {
+    "revision": "8150923869619a0ae3c6",
+    "url": "/cv-site/static/js/2.b7c28ae1.chunk.js"
+  },
+  {
+    "revision": "b551a0bf3916520b5608",
+    "url": "/cv-site/static/js/main.1359b04a.chunk.js"
+  },
+  {
+    "revision": "d3e9408399edc4fb6fdc",
+    "url": "/cv-site/static/js/runtime~main.d6de3930.js"
+  },
+  {
+    "revision": "3bb9a86db78de296b2415960731241d6",
+    "url": "/cv-site/static/media/caret-bottom.3bb9a86d.svg"
+  },
+  {
+    "revision": "5cd96532b635e8a99d80f471304f23b6",
+    "url": "/cv-site/static/media/caret-right.5cd96532.svg"
+  },
+  {
+    "revision": "141c26f2dde3f71c6069b5db139812c2",
+    "url": "/cv-site/static/media/cover-image.141c26f2.jpg"
+  },
+  {
+    "revision": "7ea4d7c5be5260c0dbde227bfd946851",
+    "url": "/cv-site/static/media/logo-doublebold-white-colour.7ea4d7c5.png"
+  },
+  {
+    "revision": "c9be86f1f1cebc6a400ee6320f28f7b4",
+    "url": "/cv-site/static/media/logo-emblem.c9be86f1.png"
+  }
+]);
