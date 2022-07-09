1e1b0334e07be552bba9d93f90dc967754f31395
V.C <vitorrc314@gmail.com>
2022-07-09 17:10:14 +0900

    update headings and summaries

--- src/components/HomeContent/Summary/SummaryHeading/SummaryHeading.js
+++ src/components/HomeContent/Summary/SummaryHeading/SummaryHeading.js
@@ -14,8 +14,8 @@ import { Container, Row, Col } from 'reactstrap';
             <Row>
                 <Col className="text-center"><p className="lead"> <span>I am a process-oriented individual with
                     a go-getter attitude and a track record of taking <Link className="initiative-link" to="/profile" onClick={() => props.onSelectSkill("Initiative")}><strong>initiative</strong></Link>.</span> <br/>
-                    I am currently based in Toronto, Canada but am looking to relocate to Tokyo,
-                    Japan and am on the lookout for job opportunities in that area.</p></Col>                   
+                    I am currently based in Tokyo, Japan
+                    and am on the lookout for new job opportunities.</p></Col>                   
             </Row>
 
         </Container>
